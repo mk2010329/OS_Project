efa4cb28f3b5cb84110e2500b678000feb4de32e
silzai <shukraan43@gmail.com>
2024-03-04 12:02:56 +0300

    added background task for clearing log with files erase_log.sh and erase_log_command.sh

---
+++ project/server_side/erase_log.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# installing cron to do scheduled tasks/processes/script runs
+sudo apt update
+sudo apt install cron
+
+# enabling cron
+sudo systemctl enable cron
+
+# command to add the erase_log_command.sh to crontab to run in the background
+(crontab -l 2>/dev/null; echo "00 12 * * 0 cd /home/basil/Desktop/OS_Project/project/server_side/ && ./erase_log_command.sh") | crontab -
---
+++ project/server_side/erase_log_command.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# this command will erase all contents of `unsuccessful_attempts.log'
+> /home/basil/Desktop/OS_Project/project/server_side/unsuccessful_attempts.log
