d1f6c09d203ae24a12ff675bd964272ee245de3f
mahodidothsp <mahodi.hsp@gmail.com>
2024-03-21 14:17:36 +0300

    helo

--- project/server_side/setups/dns_setup.sh
+++ project/server_side/setups/dns_setup.sh
@@ -6,7 +6,11 @@ sudo systemctl start resolvconf.service
 sudo systemctl enable resolvconf.service
 
 # setting the device to use Quad9's DNS service as local DNS
-echo $'nameserver 9.9.9.9\nnameserver 149.112.112.112' > etc/resolvconf/resolv.conf.d/head
+#echo $'nameserver 9.9.9.9\nnameserver 149.112.112.112' > etc/resolvconf/resolv.conf.d/head
+
+#new lines below
+sudo bash -c 'echo "nameserver 9.9.9.9" > /etc/resolvconf/resolv.conf.d/head'
+sudo bash -c 'echo "nameserver 149.112.112.112" >> /etc/resolvconf/resolv.conf.d/head'
 
 # updating the resolv.conf file
 sudo resolvconf --enable-updates
