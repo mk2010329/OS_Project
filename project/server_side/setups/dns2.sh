6e9f9bcf75054ead9a4bc5c5d0cfa228c06bdccc
mahodidothsp <mahodi.hsp@gmail.com>
2024-03-21 13:58:54 +0300

    done somethings i'm not proud of

---
+++ project/server_side/setups/dns2.sh
@@ -0,0 +1,18 @@
+# Get the system's IP address
+ip_address=$(hostname -I | awk '{print $1}')
+
+# Set the desired domain name
+domain_name="os.com"
+
+# Update the resolvconf head file with the domain name and IP address mapping
+echo "domain $domain_name" > /etc/resolvconf/resolv.conf.d/head
+echo "search $domain_name" >> /etc/resolvconf/resolv.conf.d/head
+echo "nameserver $ip_address" >> /etc/resolvconf/resolv.conf.d/head
+
+# Enable updates and update the resolv.conf file
+sudo resolvconf --enable-updates
+sudo resolvconf -u
+
+# Restart the resolvconf service
+sudo systemctl restart resolvconf.service
+sudo systemctl restart systemd-resolved.service
