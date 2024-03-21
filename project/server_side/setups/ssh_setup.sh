8cf3e9679033838ec21acd5edd9741b969e9c285
mk2010329 <mk2010329>
2024-03-21 16:24:43 +0300

    added some commands in ssh script

--- project/server_side/setups/ssh_setup.sh
+++ project/server_side/setups/ssh_setup.sh
@@ -1,7 +1,10 @@
+sudo apt update
+sudo apt upgrade
+
 #setting up SSH for server
 sudo apt install openssh-server
 
-
+sudo systemctl enable sshd
 sudo nano /etc/ssh/sshd_config
 
 #Match Group sftpgroup
@@ -10,14 +13,28 @@ sudo nano /etc/ssh/sshd_config
 #AllowTcpForwarding no
 #ForceCommand internal-sftp
 
-sudo systemctl restart sshd
 
 #setting up SFTP server
 
 $ sudo groupadd sftpgroup
 
-$ sudo useradd -G sftpgroup -d /srv/sftpuser -s /sbin/nologin sftpuser
+$ sudo useradd sftp_server
+
+$ sudo usermod -a -G sftp sftp_server
+
+$ sudo mkdir -p /var/sftp/Files
+
+$ sudo chown root:root /var/sftp
+
+$ sudo chmod 755 /var/sftp
+
+#Match User sftp_server
+#ChrootDirectory /var/sftp
+#X11Forwarding no
+#AllowTcpForwarding no
+#ForceCommand internal-sftp
 
 
+sudo systemctl restart ssh
 
 
