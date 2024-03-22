b7351606d24456b8cc97b22e44bed9dad0838d60
mahodidothsp <mahodi.hsp@gmail.com>
2024-03-22 17:09:22 +0300

    Changed main.sh a bit

--- project/client_side/main.sh
+++ project/client_side/main.sh
@@ -38,17 +38,17 @@ attempt=1
 
 while [ $attempt -le $max_attempts ]; do # $attempt holds the value of variable attempt, same for max_attempts, "-le" means less than or equal to
     echo "Attempt count: $attempt:"
-    read -p "Enter username: " username
-    read -sp "Enter password: " password
+    read -sp "Enter password for $USER: " password
 
     # Log the invalid attempt with username and timestamp
+    datestamp=$(date +"%Y-%m-%d")
     timestamp=$(date +"%Y-%m-%d_%T")
-    filename="'$username'_$timestamp'_invalid_attempts.log"
+    filename="$USER-$datestamp-invalid_attempts.log"
     echo " "
-    echo "Invalid login attempt: Username=$username, Timestamp=$timestamp" >> "$filename"
+    echo "Invalid login attempt: Username=$USER, Timestamp=$timestamp" >> "$filename"
 
     # Try to login using SSH
-     sshpass -p "$password" ssh "$username"@192.168.10.15 echo "Login successful" && break
+     sshpass -p "$password" ssh "$USER"@192.168.10.15 echo "Login successful" && break
 
     # Increment attempt counter
     ((attempt++))
@@ -60,10 +60,10 @@ if [ $attempt -gt $max_attempts ]; then
     echo " "
     echo "Unauthorized user!"
     # Copy the log file to the server using rsync
-    rsync -avz ~/Desktop/OS_Project/project/client_side/*.log server@192.168.10.15:/home/server/Desktop/OS_Project/project/server_side/user_logs #/home/hassam/Desktop/OS_Project/project/server_side/user_logs
+    sshpass -p "server" rsync -avz ~/Desktop/OS_Project/project/client_side/*.log server@192.168.10.15:/home/server/Desktop/OS_Project/project/server_side/user_logs #/home/hassam/Desktop/OS_Project/project/server_side/user_logs
     mv *.log pastlogs 
     # Schedule a user logout from the desktop session after one minute
-    #sleep 60 && gnome-session-quit --no-prompt --force
+    sleep 60 && gnome-session-quit --no-prompt --force
 fi
 
 echo "Script execution complete."
