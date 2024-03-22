bdad209b56e6a5f0249ab930771872b33321e446
lmuazkhanl <mkhan20213@gmail.com>
2024-03-22 13:41:13 +0300

    done with testing part

--- project/test/meshping.sh
+++ project/test/meshping.sh
@@ -2,7 +2,7 @@
 
 #installing nmap to use it for seeing which port are up in the network
 
-# sudo apt install nmap
+sudo apt install nmap
 
 # We use the pipe so that on the output we can use grep to do pattern matching after extracting the ips we redirect and store the result in ip.txt.
 
@@ -14,15 +14,26 @@ readarray -t arrayip <ip.txt
 #we use for loop over how much elements ib=n the array
 for ((i=0; i<${#arrayip[@]}; i++)); do
 
-	#will only send 4 packet to the destination ip address and store in /dev/null
+	#will only send 4 packet to the destination ip address and store in /dev/null we do this as we dont want the out we just want to see if the ping was successful
+	#we check this in the if statement
 	ping -c 4 ${arrayip[i]} > /dev/null
 
 	#checks if previous process was successful so will show Ping was successful
 	if [[ $? -eq 0 ]]; then
-		current_date=$(date +"%Y-%m-%d %H-%M-%S")
-    	echo -e "$current_date Connectivity with ${arrayip[i]} is ok\n"
+		current_date=$(date +"%Y-%m-%d %H:%M:%S")
+    	echo -e "$current_date Connectivity with ${arrayip[i]} is ok\n" 
+
+    	#storing the result in network.log file so we know which Ping were successful and here we are appending to the file.
+    	echo -e "$current_date Connectivity with ${arrayip[i]} is ok\n" >> networks.log
+			
 	else
-		echo -e "STOP\n"
+		val=${arrayip[i]}
+
+		#exporting the ip address in Val so we can use it in traceroute script
+		export val
+
+		#calling traceroute script
+		./traceroute.sh
 
 	fi
 
--- project/test/traceroute.sh
+++ project/test/traceroute.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-traceroute x > network.log;
-sudo reboot;
+
+# changes the value of ttl to 5 as small network also because default value is 30 so alot of waste space
+echo "Running traceroute script on $val"
+
+traceroute -m 5 $val >> networks.log
+
+echo "Rebooting..."
+sleep 10
+sudo reboot
 
