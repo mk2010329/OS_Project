e0398a245503fb8a0c0ff8f409938633312b0157
mk2010329 <mk2010329>
2024-03-23 00:19:01 +0300

    corrected the eraselog.sh and consolidatelog.sh files

--- project/server_side/erase_log.sh
+++ project/server_side/erase_log.sh
@@ -8,4 +8,10 @@ sudo apt install cron
 sudo systemctl enable cron
 
 # command to add the erase_log_command.sh to crontab to run in the background
-(crontab -l 2>/dev/null; echo "00 12 * * 0 cd /home/basil/Desktop/OS_Project/project/server_side/ && ./erase_log_command.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 12 * * 1 cd ~/Desktop/OS_Project/project/server_side/ && ./erase_log_command.sh") | crontab -
+
+#for every week:
+# 0 12 * * 1
+
+#for every minute (testing only)
+#* * * * *
