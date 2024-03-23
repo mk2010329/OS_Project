3e27497da368e7cfbf0c32f64d444e424373c6f8
mahodidothsp <mahodi.hsp@gmail.com>
2024-03-23 16:10:47 +0300

    added final changes

--- project/client_side/main.sh
+++ project/client_side/main.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#making sure client requirements are there
+echo "Enter password for sudo actions: "
+sudo apt update
+sudo apt install openssh-server
+sudo systemctl start sshd
+sudo systemctl enable sshd
 
 # Step 2: Log every invalid attempt to the server using SSH
 
@@ -8,11 +14,12 @@
 # Define the maximum number of login attempts
 max_attempts=3
 attempt=1
+address_ip="192.168.10.14"
 
 while [ $attempt -le $max_attempts ]; do # $attempt holds the value of variable attempt, same for max_attempts, "-le" means less than or equal to
     echo "Attempt count: $attempt:"
     read -sp "Enter password for $USER: " password
-
+    
     # Log the invalid attempt with username and timestamp
     datestamp=$(date +"%Y-%m-%d")
     timestamp=$(date +"%Y-%m-%d_%T")
@@ -21,7 +28,7 @@ while [ $attempt -le $max_attempts ]; do # $attempt holds the value of variable 
     echo "Invalid login attempt: Username=$USER, Timestamp=$timestamp" >> "$filename"
 
     # Try to login using SSH
-     sshpass -p "$password" ssh "$USER"@192.168.10.15 echo "Login successful" && break
+     sshpass -p "$password" ssh "$USER"@"$address_ip" echo "Login successful" && break
 
     # Increment attempt counter
     ((attempt++))
@@ -33,35 +40,35 @@ if [ $attempt -gt $max_attempts ]; then
     echo " "
     echo "Unauthorized user!"
     # Copy the log file to the server using rsync
-    sshpass -p "server" rsync -avz ~/Desktop/OS_Project/project/client_side/*.log server@192.168.10.15:/home/server/Desktop/OS_Project/project/server_side/user_logs #/home/hassam/Desktop/OS_Project/project/server_side/user_logs
-    mv *.log pastlogs 
+    sshpass -p "server" rsync -avz "$filename" server@"$address_ip":/home/server/OS_Project/project/server_side/user_logs #/home/hassam/Desktop/OS_Project/project/server_side/user_logs
+    #mv *.log pastlogs
     # Schedule a user logout from the desktop session after one minute
     sleep 60 && gnome-session-quit --no-prompt --force
 fi
-# Step 1: Check User Group Membership
+
+
+# Step 1: Check User Group Membership on the Server
 
 # Define the group name
 group_name="clients"
 
-# Check if the current user is part of the group
-if groups "$USER" | grep -q "\b${group_name}\b"; then
-    echo "User is part of the '${group_name}' group. Proceeding with script execution."
+# Check if the current user is part of the group on the server
+if sshpass -p "$password" ssh "$USER"@"$address_ip" groups | grep -q "\b${group_name}\b"; then
+    echo "User is part of the '${group_name}' group on the server. Proceeding with script execution."
 else
-    echo "User is not part of the '${group_name}' group."
+    echo "User is not part of the '${group_name}' group on the server."
 
-    # Prompting the user to obtain superuser privileges
-    echo "Please provide superuser privileges to add "$USER" to the '${group_name}' group."
-
-    # Add the user to the group (create the group if it does not exist)
-    sudo groupadd "${group_name}"
-    sudo usermod -aG "${group_name}" "$USER"
+    # Add the user to the group on the server using sudo and -S option to read the password from standard input
+    sshpass -p "$password" ssh -tt "$USER"@"$address_ip" sudo -S groupadd "${group_name}"
+    sshpass -p "$password" ssh -tt "$USER"@"$address_ip" sudo -S usermod -aG "${group_name}" "$USER"
 
     # Check if the group was successfully added
-    if groups "$USER" | grep -q "\b${group_name}\b"; then
-        echo "User successfully added to the '${group_name}' group."
+    if sshpass -p "$password" ssh "$USER"@"$address_ip" groups | grep -q "\b${group_name}\b"; then
+        echo "User successfully added to the '${group_name}' group on the server."
     else
-        echo "Failed to add user to the '${group_name}' group. Please check your superuser privileges."
+        echo "Failed to add user to the '${group_name}' group on the server. Please check your server password."
         exit 1
     fi
 fi
+
 echo "Script execution complete."
--- project/client_side/varify.sh
+++ project/client_side/varify.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
 # Create a user named "techuser" with a password.
+echo "Trying to add techuser using sudo"
 sudo useradd -m techuser
 echo "Enter password for techuser:"
 sudo passwd techuser
+
+echo "techuser ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
 # Attempt running main.sh under "techuser".
 echo "Attempting to run main.sh under techuser..."
 if sudo -u techuser ./main.sh; then
 	exit 0
 else
+
 	# Create a new group named "admins".
 	sudo groupadd admins
-
 	# Add "admins" to the sudoers file (/etc/sudoers).
 	sudo bash -c 'echo "%admins ALL=(ALL:ALL) ALL" >> /etc/sudoers'
 
@@ -22,4 +24,6 @@ else
 	# Try executing main.sh again as "techuser".
 	echo "Attempting to run main.sh again as techuser..."
 	sudo -u techuser ./main.sh
+
+
 fi
